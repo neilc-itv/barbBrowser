947661617bc33fe3ff8cbad31f4bef4a474c0710
Neil Charles <neil.charles@itv.com>
2023-03-03 14:17:19 +0000

    added basic dockerfile

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM rocker/shiny
+
+COPY app /srv/shiny-server/barbBrowser
+
+EXPOSE 8080
+
+USER shiny
+
+# avoid s6 initialization
+# see https://github.com/rocker-org/shiny/issues/79
+CMD ["/usr/bin/shiny-server"]
---
+++ deploy_cloudrun.R
@@ -0,0 +1,15 @@
+library(googleCloudRunner)
+
+repo <- cr_buildtrigger_repo("neilc-itv/barbBrowser")
+
+# deploy a cloud build trigger so each commit build the image
+cr_deploy_docker_trigger(
+  repo,
+  image = "barb-browser"
+)
+
+# deploy to Cloud Run
+cr_run(sprintf("gcr.io/%s/barb-browser:latest",cr_project_get()),
+       name = "barb-browser",
+       concurrency = 80,
+       max_instances = 1)
