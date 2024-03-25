e20d000e46151eaeddefb86347c3d9f8c66ec0e9
Neil Charles <neil.charles@itv.com>
2024-03-25 11:23:57 +0000

    title change

--- app/R/app.R
+++ app/R/app.R
@@ -38,7 +38,7 @@ barbBrowser <- function(...) {
     use_waiter(),
     waiter_on_busy(),
     
-    title = "GeoX Self-Service",
+    title = "Interactive GeoX",
     
         # href = "https://www.itvmedia.co.uk/itv-adlabs/product",
         # image = "https://storage.googleapis.com/itv-logos/adlabs.jpg"
